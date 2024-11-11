3a2043c4e77ef52c6cda003492ad9baaefd3f9f0
muttaqinmohdnoor <muttaqinwork@gmail.com>
2024-11-11 19:24:56 +0800

    a

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$database_name = "login_db";
+$database_name = "event_managment";
 $database_user = "root";
-$database_password = "password";
+$database_password = "";
 
 $connection = new mysqli("localhost", $database_user, $database_password, $database_name);
 
