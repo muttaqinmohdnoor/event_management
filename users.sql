152f63cd541fd6053a4a461db4ff571ffb1dce11
muttaqinmohdnoor <muttaqinwork@gmail.com>
2024-11-11 19:39:50 +0800

    update sql

--- users.sql
+++ users.sql
@@ -1,26 +1,14 @@
--- -------------------------------------------------------------
--- -------------------------------------------------------------
--- TablePlus 1.2.2
---
--- https://tableplus.com/
---
--- Database: login_db
--- Generation Time: 2024-11-11 16:58:04.888617
--- -------------------------------------------------------------
-
-CREATE TABLE `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `phone` varchar(50) DEFAULT NULL,
-  `address` text,
-  `city` varchar(50) DEFAULT NULL,
-  `state` varchar(50) DEFAULT NULL,
-  `zipcode` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+CREATE TABLE users (
+  id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255) DEFAULT NULL,
+  phone VARCHAR(50) DEFAULT NULL,
+  address TEXT,
+  city VARCHAR(50) DEFAULT NULL,
+  state VARCHAR(50) DEFAULT NULL,
+  zipcode VARCHAR(50) DEFAULT NULL,
+  country VARCHAR(50) DEFAULT NULL,
+  created_at DATETIME DEFAULT NULL,
+  updated_at DATETIME DEFAULT NULL
+);
